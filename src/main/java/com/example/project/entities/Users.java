f77d85ff82664aa17afb67ca430b415034f2a03f
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2022-09-05 21:24:23 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/example/project/entities/Users.java
+++ src/main/java/com/example/project/entities/Users.java
@@ -1,13 +1,15 @@
 package com.example.project.entities;
 
 import java.io.Serializable;
-//import java.util.ArrayList;
-
-import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 //import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -17,6 +19,7 @@ import javax.persistence.GenerationType;
 
 
 @Entity
+@Table(name="tb_user")
 public class Users implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -28,6 +31,9 @@ public class Users implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 /*	
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
@@ -84,12 +90,11 @@ public class Users implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-/*
+	
 	public List<Order> getOrders() {
 		return orders;
 	}
-*/	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -114,4 +119,6 @@ public class Users implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 }
