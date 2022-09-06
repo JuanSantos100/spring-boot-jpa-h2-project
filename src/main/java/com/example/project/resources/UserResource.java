84afd8240f8fdbd9b5bb8bef866f1b4328f12fd0
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2022-09-05 21:06:28 -0300

    Service Layer, component registration

--- src/main/java/com/example/project/resources/UserResource.java
+++ src/main/java/com/example/project/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.example.project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.project.entities.Users;
+import com.example.project.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserServices service;
+	
 	@GetMapping
-	public ResponseEntity<Users> findAll() {
-		
-		return null;
+	public ResponseEntity<List<Users>> findAll() {
+		List <Users> listaUsuarios = service.findAll();
+		return ResponseEntity.ok().body(listaUsuarios);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Users> findById(@PathVariable Long id) {
+		Users obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/example/project/services/UserServices.java
@@ -0,0 +1,26 @@
+package com.example.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.project.entities.Users;
+import com.example.project.repositories.UserRepository;
+
+@Service //Registro de componente da classe que sofrerá injecção de dependência
+public class UserServices {
+	
+	@Autowired //Spring faz a injeção de dependência de forma transparente ao programador
+	private UserRepository repository;
+	
+	public List<Users> findAll() {
+		return repository.findAll();
+	}
+	
+	public Users findById(Long id) {
+		Optional <Users> obj = repository.findById(id);
+		return obj.get(); //Retorna um objeto do tipo Users
+	}
+}
